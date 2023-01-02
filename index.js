8efa4041a4cb2c9309540ba3d9fd028ef193be0b
Ali Hasnain <abdulrehman76261@gmail.com>
2023-01-02 07:09:59 -0800

    mongoose crud

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
-const getData = require('./mongodb')
-const main = async () => {
-   let data = await getData();
-   data = await data.find().toArray();
-   console.log(data);
-}
+const mongoose = require('mongoose');
 
-main();
-console.log(getData());
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/e-comm");
+const  ProductSch = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
+});
+const saveInDB = async() => {
+
+    const ProductModel = mongoose.model('product', ProductSch);
+    let data = await new ProductModel(
+        {
+            name: "m8",
+            price: 3434,
+            brand: "haper",
+            category: "koper"
+
+        }
+        
+        );
+    console.log(data)
+    let result  = await data.save();
+    console.log(result)
+
+};
+
+const updateInDb = async () => {
+    mongoose.set("strictQuery", false);
+    const Product = mongoose.model('product', ProductSch);
+    let data = await Product.updateOne(
+        {
+            name: "m8"
+        },
+        {
+            $set: {price: 7943574834}
+        }
+        
+        )
+        console.log(data)
+    }
+    
+    const deleteInDb = async () => {
+    const Product = mongoose.model('product', ProductSch);
+    let data  = await Product.deleteOne(
+        {
+            name: "m8"
+        }
+
+        )
+}
+    const findInDb = async () => {
+    const Product = mongoose.model('product', ProductSch);
+    let data  = await Product.find()
+    console.log(data)
+}
+// updateInDb()
+// saveInDB()
+findInDb();
